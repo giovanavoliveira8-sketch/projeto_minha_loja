b954d400c6f72a6284a29360958692d62d16b5ee
Giovana <Giovana.v.oliveira8@aluno.senai.br>
2026-07-09 16:47:54 -0300

    Modificado

--- js/produtos.js
+++ js/produtos.js
@@ -7,5 +7,33 @@ const sectioCards = document.querySelector('#cards')
 //CARREGANDO OS CARDS
 const listarProdutos = () => {
 
+    produtos.forEach((elem, i)=>{
+        const divCard= document.createElement('div')
+        divCard.setAttribute('class', 'card')
+    
+        const imgCard= document.createElement('img')
+        imgCard.setAttribute('src', elem.caminho_imagem)
+        imgCard.setAttribute('alt', elem.descricao_produto)
+    
+        const pCard = document.createElement('p')
+        pCard.innerHTML = elem.descricao_produto 
+    
+        const h2Card = document.createElement('h2')
+        h2Card.innerHTML `R$ ${parseFloat(elem.valor_unitario).toFixed (2).replace('.',',')}`
+    
+        const btnCard = document.createElement('button')
+        btnCard.setAttribute('class', 'btn-add')
+        btnCard.innerHTML = 'Adicionar'
+
+        divCard.appendChild(imgCard)
+        divCard.appendChild(pCard)
+        divCard.appendChild(h2Card)
+        divCard.appendChild(btnCard)
+
+        sectioCards.appendChild(divCard)
+
+    })
+
 }
 
+listarProdutos()
