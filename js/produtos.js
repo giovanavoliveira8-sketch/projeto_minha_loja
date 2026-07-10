b2609a1ba7899742c5281e8c57fe27f1a397b7ba
Giovana <giovana.v.oliveira8@aluno.senai.br>
2026-07-10 13:53:23 -0300

    Comentando as linhas da função

--- js/produtos.js
+++ js/produtos.js
@@ -7,33 +7,47 @@ const sectioCards = document.querySelector('#cards')
 //CARREGANDO OS CARDS
 const listarProdutos = () => {
 
+
+    //LIMPANDO A SECTION cards
+    sectioCards.innerHTML = ''
+
+    //PERCORRENDO O ARRAY DE PRODUTOS
     produtos.forEach((elem, i)=>{
+        //CRIANDO O ELEMNTO div E DEFININDO O ATRIBUTO CARD
         const divCard= document.createElement('div')
         divCard.setAttribute('class', 'card')
     
+        //CRIANDO O ELEMENTO img E DEFININDO OS ATRIBUTOS SRC E ALT OS VALORES DO CAMINHO DAS IMAGENS E A DESCRIÇÂO DOS PRODUTOS
         const imgCard= document.createElement('img')
         imgCard.setAttribute('src', elem.caminho_imagem)
         imgCard.setAttribute('alt', elem.descricao_produto)
     
+        //CRIANDO O ELEMENTO p E ATRIBUINDO A DESCRIÇÃO DOS PRODUTOS 
         const pCard = document.createElement('p')
         pCard.innerHTML = elem.descricao_produto 
     
+        //CRIANDO O ELEMENTO h2 E ATRIBUINDO O VALOR UNITÁRIO DEIXANDO EM DUAS CASAS DECIMAIS E SUBSTITUINDO PONTO POR VÍRGULA
         const h2Card = document.createElement('h2')
         h2Card.innerHTML `R$ ${parseFloat(elem.valor_unitario).toFixed (2).replace('.',',')}`
     
+        //CRIANDO O ELEMENTO button E DEFININDO OS ATRIBUTOS CLASS E A DESCRIÇÃO ADICIONAR
         const btnCard = document.createElement('button')
         btnCard.setAttribute('class', 'btn-add')
         btnCard.innerHTML = 'Adicionar'
 
+
+        //ADICIONANDO OS ELEMENTOS FILHOS AOS divCard
         divCard.appendChild(imgCard)
         divCard.appendChild(pCard)
         divCard.appendChild(h2Card)
         divCard.appendChild(btnCard)
 
+        //ADICIONANDO O divCard A SECTION CARDS
         sectioCards.appendChild(divCard)
 
     })
 
 }
 
+//CHAMANDO A FUNÇÃO listarProdutos
 listarProdutos()
