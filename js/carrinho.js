d381c9e7c8673dce8f06a628b19f32c3099ff8ae
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-07-14 16:51:19 -0300

    comitado

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,28 +1,23 @@
-const precos = [95, 50, 15, 100];
-const frete = 10;
+//CRIANDO ARRAY DE ITENS DO CARRINHO
+let itenscarrinho = JSON.parse(sessionStorage.getItem("carrinhoSessao")) || []
+//const itensCarrinho02 = JSON.parse(sessionStorage.get("carrinhoSessao")) || []
 
-const inputs = document.querySelectorAll(".produto input");
+//FUNÇÃO PARA ADICIONAR ITEM
+const addItem = (objItem) => {
+    itenscarrinho.push(objItem)
 
-function atualizarTotal() {
+    sessionStorage.setItem("carrinhoSessao", JSON.stringify(itenscarrinho))
 
-    let total = 0;
-
-    inputs.forEach((input, indice) => {
-        total += Number(input.value) * precos[indice];
-    });
-
-    document.getElementById("valorTotal").innerHTML =
-        "Valor Total: R$ " + total.toFixed(2);
-
-    document.getElementById("valorFrete").innerHTML =
-        "Frete: R$ " + frete.toFixed(2);
-
-    document.getElementById("valorPagar").innerHTML =
-        "Total: R$ " + (total + frete).toFixed(2);
 }
 
-inputs.forEach(input=>{
-    input.addEventListener("input", atualizarTotal);
-});
+//FUNÇÂO PARA LISTAR OS ITENS DO CARRINHO
+const listItens = () => {
+    const listaItens = JSON.parse(sessionStorage.getItem("carrinhoSessao")) || []
 
-atualizarTotal();
+    return listaItens
+
+}
+
+
+//EXPORTAÇÃO
+export {addItem, listItens}
