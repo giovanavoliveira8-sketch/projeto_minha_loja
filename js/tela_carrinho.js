c8deadac385be1f2c712a96a57a7bd5b61d2b5ac
Giovana <Giovana.v.oliveira8@aluno.senai.br>
2026-07-15 14:15:14 -0300

    Modificado

---
+++ js/tela_carrinho.js
@@ -0,0 +1,71 @@
+import { listItens, removeItem } from './carrinho.js'
+
+const montaTelaCarrinho = () => {
+    const divProdutos = document.querySelector('#produtos')
+
+    divProdutos.innerHTML = ''
+
+    listItens().forEach((elem, i) => {
+        const sectionItem = document.createElement('section')
+        sectionItem.setAttribute('class', 'produto')
+
+        const imgItem = document.createElement('img')
+        imgItem.setAttribute('src', elem.caminhoImagem)
+        imgItem.setAttribute('alt', elem.descricaoProduto)
+
+        sectionItem.appendChild(imgItem)
+
+        const divDescricaoItens = document.createElement('div')
+        divDescricaoItens.setAttribute('class', 'descricao-itens')
+
+        const divDescricao = document.createElement('div')
+        divDescricao.setAttribute('class', 'descricao')
+        divDescricao.innerHTML = elem.descricaoProduto
+
+        const divValores = document.createElement('div')
+        divValores.setAttribute('class', 'valores')
+
+        const pItem = document.createElement('p')
+        pItem.innerHTML = `R$ ${parseFloat(elem.valorUnitario).toFixed
+        (2).replace('.', '.')}`
+
+        const divQuant = document.createElement('div')
+        divQuant.setAttribute('class', 'input-quantidade')
+
+        const inputQuantidade = document.createElement('input')
+        inputQuantidade.setAttribute('type', 'number')
+        inputQuantidade.setAttribute('name', `quant${i}`)
+        inputQuantidade.setAttribute('id', `quant${i}`)
+        inputQuantidade.setAttribute('class', 'input-item')
+        inputQuantidade.setAttribute('value', 1)
+
+        divQuant.appendChild(inputQuantidade)
+
+        const pCalc = document.createElement('p')
+        pCalc.innerHTML = `R$ ${elem.valorUnitario * 1}`
+
+        const imgRemover = document.createElement('img')
+        imgRemover.setAttribute('src', '../imagens/icones/remover.png')
+        imgRemover.setAttribute('alt', 'remover')
+
+        imgRemover.addEventListener('click', () => {
+            removeItem(i);
+            montaTelaCarrinho();
+        });
+
+        divValores.appendChild(pItem)
+        divValores.appendChild(divQuant)
+        divValores.appendChild(pCalc)
+        divValores.appendChild(imgRemover)
+         
+        divDescricaoItens.appendChild(divDescricao)
+        divDescricaoItens.appendChild(divValores)
+
+        sectionItem.appendChild(divDescricaoItens)
+
+        divProdutos.appendChild(sectionItem)
+
+    })
+}
+
+montaTelaCarrinho()
